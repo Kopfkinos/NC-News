21b2a47008a68b74c9995b18cad27cc45e1f44b7
IgelIntelligenz <connor_v_d_murphy@hotmail.com>
2025-03-13 15:12:47 +0000

    this commit will edit the connection, listener and package.json to allow these apis to be hosted online via Supabase

--- listener.js
+++ listener.js
@@ -1,9 +1,5 @@
 const app = require("./app")
 
-app.listen(9090, (err) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log("Reading you live and clear on our dev port Ulala...")
-  }
-})
+const { PORT = 9090 } = process.env
+
+app.listen(PORT, () => console.log(`Reading you loud and clear on ${PORT}...`))
